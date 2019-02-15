d0e927f757ce935c08f4a52e864a904be2eb3b7c
UTKARSH KUMAR <36263064+utkarsh137@users.noreply.github.com>
2019-02-16 01:07:58 +0530

    Serial Display on LCD
    Whatever characters arrive over serial port these characters display on the LCD

---
+++ Serial_Display.ino
@@ -0,0 +1,30 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F,16,2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
+int a=6;
+int b=3;
+void setup()
+{
+  lcd.begin();                      // initialize the lcd 
+  lcd.backlight();
+  Serial.begin(9600);
+  
+}
+
+void loop()
+{
+
+   //when characters arrive over the serial port...
+  if (Serial.available()) {
+    // wait a bit for the entire message to arrive
+    delay(100);
+    // clear the screen
+    lcd.clear();
+    // read all the available characters
+    while (Serial.available() > 0) {
+      // display each character to the LCD
+      lcd.write(Serial.read());
+    }
+  }
+}
